8a30eadab208126535abcc9355b630fff35eebb0
majsky <majsky4@hotmail.com>
2013-05-04 20:07:40 +0200

    Fixed and working now

--- src/me/majsky/Home/HomePlugin.java
+++ src/me/majsky/Home/HomePlugin.java
@@ -3,9 +3,10 @@ package me.majsky.Home;
 import java.util.HashMap;
 import java.util.Map;
 
-import me.majsky.lib.ChatUtils;
 import me.majsky.lib.DataSaver;
-import me.majsky.lib.ParticleUtils;
+import me.majsky.lib.LocationConverter;
+import me.majsky.lib.util.ChatUtils;
+import me.majsky.lib.util.ParticleUtils;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
@@ -17,33 +18,34 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class HomePlugin extends JavaPlugin{
 
-    public Map<String, SavableLocation> homes;
+    public Map<String, String> homes;
     
     @Override
     public void onDisable() {
-        DataSaver.save(homes, "homes", this);
+        DataSaver.saveReadable(homes, "homes", this);
     }
-
-    @SuppressWarnings("unchecked")
+    
     @Override
     public void onEnable() {
         if(DataSaver.isPresent("homes", this))
-            homes = (Map<String, SavableLocation>) DataSaver.load("homes", this);
+            homes = DataSaver.loadReadable("homes", this);
         else
-            homes = new HashMap<String, SavableLocation>();
+            homes = new HashMap<String, String>();
     }
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(!(sender instanceof Player))
+        if(!(sender instanceof Player)){
+            sender.sendMessage("You must be in-game!");
             return false;
+        }
         
         Player p = (Player)sender;
         
         switch (command.getName().toLowerCase()) {
             case "sethome":
                 ChatUtils.sendMessaegeAndSound(p, ChatColor.GOLD + "Home nadstaveny!", Sound.CLICK);
-                homes.put(p.getName(), new SavableLocation(p.getLocation()));
+                homes.put(p.getName(), LocationConverter.encode(p.getLocation()));
                 return true;
             case "home":
                 if(!homes.containsKey(p.getName())){
@@ -51,7 +53,7 @@ public class HomePlugin extends JavaPlugin{
                     return false;
                 }
                 ParticleUtils.createParticlesWithSound(p.getWorld(), p.getLocation(), Effect.SMOKE, 0, Sound.ENDERMAN_TELEPORT, 1, 3);
-                p.teleport(homes.get(p.getName()).getLocation());
+                p.teleport(LocationConverter.decode(homes.get(p.getName())));
                 ParticleUtils.createParticlesWithSound(p.getWorld(), p.getLocation(), Effect.SMOKE, 0, Sound.ENDERMAN_TELEPORT, 1, 3);
                 return true;
             case "spawn":
